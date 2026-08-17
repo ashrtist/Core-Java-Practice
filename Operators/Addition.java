ecf3b3f8642c770d3c538c15589ff1261fe506ad
Ashwin Gour <ashwingour404@gmail.com>
2026-08-17 13:05:20 +0530

    added txt file

---
+++ Operators/Addition.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Additon of "+a+" and "+ b+" is ="+(a+b));
+    }
+}
